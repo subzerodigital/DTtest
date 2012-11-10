e764f888d269ef92af568cf92b8a5c065c85ea17
biejieaninna <stanley_yangyinan@hotmail.com>
2012-11-10 16:57:36 +1100

    carousel fixed

--- js/app/jquery-dtCarousel-0.0.1.js
+++ js/app/jquery-dtCarousel-0.0.1.js
@@ -18,8 +18,11 @@
 	var $ = window.jQuery;
 	
 	//settings
+	//TODO:more custom settings
 	var defaults = {
-		autoPlay:false	
+		autoPlay:false,
+		interval:5000,
+		fadeSpeed:500
 	}
 	
 	//constructor
@@ -31,7 +34,8 @@
 		this.$buttons = this.$el.find(".control li a");
 		this.$slides = this.$el.find(".slides .slide");
 		
-		
+		this.currentSlide = 0;
+		this.totalSlide = this.$slides.length; 
 		
 		this.init();
 	}
@@ -45,32 +49,49 @@
 			
 			//bind click event
 			this.bind();
-			
+			//auto play
+			if(this.options.autoPlay){
+				this.startTimer();
+			}
 		},
 		
 		bind:function(){
 			var self = this;
 			
 			self.$buttons.click(function(e){
+				
 				e.preventDefault();
+				
 				//get the index of the clicked button
 				var index = self.$buttons.index(this);
+				
+				//cache current slide index
+				self.currentSlide = index;
 				
 				//if click on the current active button, do nothing
 				if($(this).hasClass("active")){
 					return false;
 				}
 				
+				//if auto play, on click stop the timer
+				if(self.options.autoPlay){
+					self.stopTimer();
+				}
+				
+				//change button states
 				self.$buttons.removeClass("active");
 				$(this).addClass("active");
+				//
 				self.fadeInSlide(index);
+				
+				if(self.options.autoPlay){
+					self.startTimer();
+				}
 			});
 			
 		},
 		
 		fadeInSlide:function(index){
-			
-			
 			
 			var $currentSlide = this.$el.find(".slide.current");			
 			
@@ -81,6 +102,25 @@
 			$nextSlide.css({opacity:0.0}).addClass("current").animate({opacity:1.0},500,function(){
 				$currentSlide.removeClass("previous");
 			});
+		},
+		
+		startTimer:function(){
+			var self = this;
+			self.carouselTimer = setInterval(function(){
+				self.currentSlide+=1;
+				//loop the slides
+				if(self.currentSlide>=self.totalSlide){
+					self.currentSlide = 0;
+				}
+				
+				self.$buttons.eq(self.currentSlide).trigger("click");
+				
+			},self.options.interval);
+		},
+		
+		stopTimer:function(){
+			var self = this;
+			window.clearInterval(self.carouselTimer);
 		}
 		
 	}
--- js/appInit.js
+++ js/appInit.js
@@ -1,5 +1,5 @@
 $(function(){
 	
 	
-	$(".fn-carousel").dtCarousel();
+	$(".fn-carousel").dtCarousel({autoPlay:true,interval:3000,fadeSpeed:200});
 })
