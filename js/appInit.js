ec9aa21ae26468dd68675ad007263d1b1df5257d
unknown <yinan.yang@optusnet.com.au>
2012-11-12 15:53:45 +1100

    some updates

--- js/appInit.js
+++ js/appInit.js
@@ -1,3 +1,8 @@
-$(function(){
+(function(window,undefined){
+	//cache jQuery
+	var $ = window.jQuery;
+	//init carousel
 	$(".fn-carousel").dtCarousel({autoPlay:true,interval:3000,fadeSpeed:500});
-})
+	//some other bindings eg. sticky footer
+})(window);
+	
